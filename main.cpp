962a27a2ccf3906d8a279b2bbd02c5c49677a02e
Caleb Dennis <caleb.dennis429@gmail.com>
2021-07-15 16:17:13 -0600

    changed behavior of func_cmd.exe    - two modes, to test function interpretation and evaluation

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include "FuncInterp.hpp"
 
-int main(){
+
+
+void inspectMode(){
    char input[100];
    while(1){
       string errmsg;
@@ -18,7 +20,34 @@ int main(){
          std::cout << std::endl;
       }
       delete func;
+   }
+}
 
+void evaluateMode(){
+   char input[100];
+   string errmsg;
+   std::cin.getline(input, 100);
+   FuncTree *func = FuncTree::fromString(std::string(input), &errmsg);
+   if(func == NULL){
+      std::cout << errmsg << std::endl;
+   }
+   else{
+      while(1){
+         std::cin.getline(input, 100);
+         if(input[0] == 'q')
+            break;
+         std::cout << func->evaluate(std::stod(input)) << std::endl;
+      }
+   }
+   delete func;
+}
+
+int main(int argc, char **argv){
+   if(argc > 1 && argv[1][0] == 'i'){
+      inspectMode();
+   }
+   else{
+      evaluateMode();
    }
    return 0;
 }
